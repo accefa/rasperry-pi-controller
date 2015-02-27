362cbb4a1728390176d1191f664921ebf5aee3b5
Alexander Suter <alexander.suter@gmx.ch>
2015-02-27 13:01:19 +0100

    sample picamera

---
+++ samples/picamera/PicameraSample.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+'''
+Created on 21.02.2015
+
+@author: Fabian Wüthrich
+'''
+
+# from picamera import PiCamera
+
+  #picam = PiCamera()
+        #picam.resolution = (1280, 720)
+        #picam.exposure_compensation = 2
+        #picam.exposure_mode = 'spotlight'
+        #picam.meter_mode = 'matrix'
+        #picam.image_effect = 'gpen' 
+        # Give the camera some time to adjust to conditions
+        #picam.sleep(2)
+        #picam.capture('foo.jpg')
+
+
+if __name__ == '__main__':
+    pass
