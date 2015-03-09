8e90fc662a850a0a1232cb1dcf383234af0ba9b7
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2015-03-09 12:41:54 +0100

    Image Resource gibt Bild zurück

--- test/webserver/resource/image_test.py
+++ test/webserver/resource/image_test.py
@@ -3,6 +3,9 @@ from paste.fixture import TestApp
 from webserver.main import app
 from nose.tools import *
 
+VALID_IMAGE_NAME = 'test_image.jpg'
+INVALID_IMAGE_NAME = 'invalid_image.jpg'
+
 
 class ImageTest(unittest.TestCase):
     def setUp(self):
@@ -10,8 +13,13 @@ class ImageTest(unittest.TestCase):
         self.testApp = TestApp(app.wsgifunc(*middleware))
 
     def test_image_get(self):
-        request = self.testApp.get("/image")
+        request = self.testApp.get('/image/' + VALID_IMAGE_NAME)
         assert_equals(request.status, 200)
+        assert_equals(request.header_dict['content-type'], 'images/jpeg')
+
+    def test_image_get_invalid_path(self):
+        request = self.testApp.get('/image/' + INVALID_IMAGE_NAME, expect_errors=True)
+        assert_equals(request.status, 404)
 
 
 if __name__ == "__main__":
--- webserver/resource/image.py
+++ webserver/resource/image.py
@@ -1,13 +1,29 @@
 import web
+import os
 
 urls = (
-    "(.*)", "Image"
+    "/(.*)", "Image"
 )
 
 app_image = web.application(urls, locals())
 
+IMAGE_FILE_PATH = os.path.dirname(__file__) + '/../../images'
+
 
 class Image:
-    def GET(self, path):
-        web.header('Content-type', 'text/html')
-        return "GET Image"
+    def GET(self, file_name):
+        extension = file_name.split(".")[-1]
+
+        file_path = os.path.join(IMAGE_FILE_PATH, file_name)
+
+        content_type = {
+            "png": "images/png",
+            "jpg": "images/jpeg",
+            "gif": "images/gif",
+            "ico": "images/x-icon"}
+
+        if file_name in os.listdir(IMAGE_FILE_PATH):
+            web.header("Content-Type", content_type[extension])
+            return open(file_path, "rb").read()
+        else:
+            raise web.notfound()
