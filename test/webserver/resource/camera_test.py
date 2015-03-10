9265629e1b14f0b38e9a5e44650b98dfa441fa14
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2015-03-11 00:23:08 +0100

    Kamera Resource an neue Config angepasst

--- test/webserver/resource/camera_test.py
+++ test/webserver/resource/camera_test.py
@@ -2,8 +2,6 @@ import json
 from paste.fixture import TestApp
 import unittest
 from webserver.main import app
-from webserver.resource.camera import CONFIG_FILE_PATH
-
 
 class CameraTest(unittest.TestCase):
     def __init__(self, method_name):
@@ -16,31 +14,18 @@ class CameraTest(unittest.TestCase):
         self.testApp = TestApp(app.wsgifunc(*middleware))
 
     def test_camera_get(self):
-        self.write_valid_json_to_config()
-        r = self.testApp.get('/camera')
-        self.assertEquals(r.status, 200)
-        r.mustcontain(self._validJSON)
-
-    def test_camera_get_invalid_json(self):
-        self.write_invalid_json_to_config()
-        r = self.testApp.get('/camera', expect_errors=True)
-        self.assertEquals(r.status, 500)
+        request = self.testApp.get('/camera')
+        self.assertEquals(request.status, 200)
+        self.assertEquals(request.header_dict['content-type'], 'text/json')
 
     def test_camera_put_invalid_json(self):
-        r = self.testApp.put('/camera', self._invalidJSON, expect_errors=True)
-        self.assertEquals(r.status, 406)
+        request = self.testApp.put('/camera', self._invalidJSON, expect_errors=True)
+        self.assertEquals(request.status, 406)
 
     def test_camera_put_json(self):
-        r = self.testApp.put('/camera', self._validJSON, expect_errors=True)
-        self.assertEquals(r.status, 200)
-
-    def write_valid_json_to_config(self):
-        with open(CONFIG_FILE_PATH, 'w') as configFile:
-            configFile.write(self._validJSON)
-
-    def write_invalid_json_to_config(self):
-        with open(CONFIG_FILE_PATH, 'w') as configFile:
-            configFile.write(self._invalidJSON)
+        request = self.testApp.put('/camera', self._validJSON, expect_errors=True)
+        self.assertEquals(request.status, 200)
+        self.assertEquals(request.header_dict['content-type'], 'text/json')
 
 
 if __name__ == "__main__":
--- webserver/resource/camera.py
+++ webserver/resource/camera.py
@@ -16,31 +16,26 @@ CONFIG_FILE_PATH = 'config.json'
 class Camera:
     def GET(self, path):
         try:
-            with open(CONFIG_FILE_PATH) as config_file:
-                config_dict = json.load(config_file)
-
+            config_dict = DetectionConfig().get_as_dict()
             web.header('Content-type', 'text/json')
             web.ok()
             return json.dumps(config_dict)
         except (TypeError, ValueError) as e:
-            print e.message
-            web.header('Content-type', 'text/json')
+            web.header('Content-type', 'text/html')
             web.internalerror()
+            return e.message
 
     def PUT(self, path):
         try:
-            json_data = web.data()
-            config_dict = json.loads(json_data)
+            config_dict = json.loads(web.data())
 
             detection_config = DetectionConfig()
             detection_config.set_from_dict(config_dict)
 
-            with open(CONFIG_FILE_PATH, 'w') as config_file:
-                json.dump(detection_config.get_as_dict(), config_file, indent=2)
-
             web.header('Content-type', 'text/json')
             web.ok()
+            return json.dumps(detection_config.get_as_dict())
         except (TypeError, ValueError) as e:
-            print e.message
-            web.header('Content-type', 'text/json')
+            web.header('Content-type', 'text/html')
             web.notacceptable()
+            return e.message
