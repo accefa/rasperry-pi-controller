20e7b25134f5053884d9ab3585b5c641de021ee6
Alexander Suter <alexander.suter@gmx.ch>
2015-03-14 00:29:25 +0100

    close camera source correctly

--- detection/Camera.py
+++ detection/Camera.py
@@ -8,13 +8,17 @@ from PIL import Image
 class CameraController(object):
     def shoot(self, detectConfig):
         stream = io.BytesIO()
+        try:
+            self.picamera = PiCamera();
+            self.picamera.contrast = detectConfig.contrast
+            self.picamera.resolution = (1280, 720) # Come on we define a fixed size
+            if detectConfig.greyscale:
+                self.picamera.color_effects = (128, 128) # grayscale
+            self.picamera.capture(stream, "jpeg", False, False, 0, quality=detectConfig.quality)
+            stream.seek(0) # "Rewind" the stream to the beginning so we can read its content
+            image = Image.open(stream)
+            return image
+        finally:
+            self.picamera.close()
         
-        self.picamera = PiCamera();
-        self.picamera.contrast = detectConfig.contrast
-        self.picamera.resolution = (1280, 720) # Come on we define a fixed size
-        if detectConfig.greyscale:
-            self.picamera.color_effects = (128, 128) # grayscale
-        self.picamera.capture(stream, "jpeg", False, False, 0, quality=detectConfig.quality)
-        stream.seek(0) # "Rewind" the stream to the beginning so we can read its content
-        image = Image.open(stream)
-        return image
+        return None
