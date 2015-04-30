524870d45d91bdbf2252de6bbc50f68ca63302c8
alexander.suter <alexander.suter.ch@gmail.com>
2015-04-30 11:15:26 +0200

    logoutputs ergänzt

--- piserial/util/command.py
+++ piserial/util/command.py
@@ -20,9 +20,11 @@ def __send_to_serial(cmd, ser):
     ser.write(cmd.encode('utf-8'))
     time.sleep(1)
     answer = ser.readline()
+    print("start waiting")
     while answer.find(term.encode('utf-8')) < 0:
         answer = answer.decode('utf-8')
         answer = answer.replace('\n', '')
+        print("still waiting");
         if answer != '':
             print("\t" + answer)
         answer = ser.readline()
